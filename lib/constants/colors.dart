32d7a62d42e6dfd7ef88e8821ee98203532bd1c1
Sidharth K V <sidharthkv1100@gmail.com>
2026-01-02 12:26:38 +0530

    Create colors.dart

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const gradientStart = Color(0xff74b9ff);
+  static const gradientEnd = Color(0xffa29bfe);
+  static const cardColor = Colors.white24;
+}
